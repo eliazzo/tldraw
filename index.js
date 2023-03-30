502e4edc882e0888c02f3ecb5aa0d15b043d21f8
eliazzo <bethazz@hotmail.co.uk>
2023-03-30 15:46:26 +0100

    bug fix attempt

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const whiteboard = document.getElementById("whiteboard");
 
 /* -------------- */
 
-function dragStart(event) { 
+function dragStart(event) {
   console.log("dragging...");
-  event.dataTransfer.setData("text", event.target.innerText);
+  event.dataTransfer.setData("text", event.target.innerText); // dataTransfer holds event data // setData manages the dragged data (type_of_data, data)
 }
-// dataTransfer holds event data
-// setData manages the dragged data (type_of_data, data)
 
 stickers.forEach((sticker) => {
   sticker.addEventListener("dragstart", dragStart); // event fires on dragstart
@@ -19,20 +17,19 @@ stickers.forEach((sticker) => {
 function dragOver(event) {
   console.log("dragging over");
   event.preventDefault(); // default behaviour does not allow drop
-} 
- 
+}
+
 whiteboard.addEventListener("dragover", dragOver); // event fires repeatedly when sticker is over whiteboard
 
 /* -------------- */
 
-function drop(event){
+function drop(event) {
   event.preventDefault(); // default behaviour opens link
-  const draggableElementData = event.dataTransfer.getData("text"); // get all data set as type 'text' by the setData method
-  const x = event.clientX - whiteboard.offsetLeft; // event.clientNum returns the coordinates of the mouse
-  const y = event.clientY - whiteboard.offsetTop; //  offset values are needed to calculate the position of the mouse click relative to the droppable element
-
-  // const draggableElement = document.getElementById(draggableElementData);
-  event.target.insertAdjacentHTML("afterbegin", `<div style="position: absolute; top: ${y}px; left: ${x}px;">${draggableElementData}</div>`);
+  const stickerData = event.dataTransfer.getData("text"); // get all data set as type 'text' by the setData method
+  const x = event.clientX;
+  const y = event.clientY;
+  const stickerDrop = `<div style="position: absolute; top: ${y}px; left: ${x}px;">${stickerData}</div>`;
+  event.target.insertAdjacentHTML("afterbegin", stickerDrop);
 }
 
-whiteboard.addEventListener("drop", drop) // handles transferred data
+whiteboard.addEventListener("drop", drop); // handles transferred data
